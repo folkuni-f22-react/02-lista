caa33c6d23f08bce899a7752b733b45e056b1f5c
David Andersson <lejonmanen@gmail.com>
2023-03-29 11:18:59 +0200

    Lista med props

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
+import ViewUser from './components/ViewUser'
 import './App.css'
 
 function App() {
 	const [count, setCount] = useState(0)
 
+	let data = [
+		{ name: 'David', email: 'test@test.se' },
+		{ name: 'Greta', email: 'greta@test.se' }
+	]
+
 	return (
 		<div className="App">
-
+		{data.map(user => (
+			<ViewUser key={user.email} name={user.name} email={user.email} />
+		))}
 		</div>
 	)
 }
+
+
 
 export default App
---
+++ src/components/ViewUser.jsx
@@ -0,0 +1,9 @@
+
+const ViewUser = ({ name, email }) => (
+	<div className="view-user">
+		<p> {name} </p>
+		<p> {email} </p>
+	</div>
+)
+
+export default ViewUser
